3a887a7b103f2b374364fd5311d65fd96b611810
Marco Camponeschi <commandermarco@hotmail.it>
2017-07-27 09:08:44 +0200

    added combined view - only normal and night vision

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -9,10 +9,14 @@ public class InputManager : MonoBehaviour {
 	private NightVision nightVisionScript;
 	private ThermalVision thermalVisionScript;
 
+	public GameObject thumbnailCameras;
+
 	void Start()
 	{
 		nightVisionScript = GetComponent<NightVision>();
 		thermalVisionScript = GetComponent<ThermalVision>();
+
+		DeactivateAllVisions();
 	}
 
 	void Update () 
@@ -33,10 +37,17 @@ public class InputManager : MonoBehaviour {
 			DeactivateAllVisions(); 
 			thermalVisionScript.enabled = true;
 		}
+
+		if(Input.GetKey(KeyCode.Alpha0))
+		{
+			DeactivateAllVisions(); 
+			thumbnailCameras.SetActive(true);
+		}			
 	}
 
 	private void DeactivateAllVisions()
 	{
+		thumbnailCameras.SetActive(false);
 		nightVisionScript.enabled = false;
 		thermalVisionScript.enabled = false;
 	}
